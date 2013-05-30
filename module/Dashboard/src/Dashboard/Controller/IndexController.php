3a5d77f43e4ddf59dcfa1db98e931427c505da62
brian978 <dbrian89@yahoo.com>
2013-05-30 21:15:18 +0300

    Work in progress - testing autoupdater

--- module/Dashboard/src/Dashboard/Controller/IndexController.php
+++ module/Dashboard/src/Dashboard/Controller/IndexController.php
@@ -17,7 +17,7 @@ class IndexController extends AbstractUiController
     public function updateAction()
     {
         // Updating the application
-        var_dump(shell_exec('pwd && git pull'));
+        var_dump(shell_exec('git pull'));
         die();
 
         $this->redirect()->toRoute('home');
